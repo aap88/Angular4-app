48949d8ac71efc66f2e8c458f5a06594a76d6bba
aap88 <adami.alessandra@hotmail.com>
2019-06-22 10:03:57 -0300

    All changes until now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { MyserviceService } from './myservice.service';
+import { mapToMapExpression } from '@angular/compiler/src/render3/util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular4-app Ale';
+  todaydate;
+  componentproperty;
+  constructor(private http:Http) {}
+  ngOnInit() {
+    this.http.get("http://jsonplaceholder.typicode.com/users").map((response) => response.json()).subscribe((data) => console.log(data))
+    //this.todaydate = this.myservice.showTodayDate();
+    //console.log(this.myservice.serviceproperty);
+    //this.myservice.serviceproperty = "component created";
+    //this.componentproperty = this.myservice.serviceproperty;
+  }
+  jsonval = {name: 'Rox', age:'25', address:{a1:'Mumbai', a2:'Karnataka'}};
+  // declared array of months.
+  months = ["January", "Feburary", "March", "April", "May", 
+  "June", "July", "August", "September",
+  "October", "November", "December"];
+  isavailable = true;
+
+  myClickFunction(event) {
+    alert("Buttons is clicked!");
+    console.log(event);
+  }
+
+  changemonths(event) {
+    //console.log("Changed month from the Dropdown");
+    //console.log(event);
+    alert("Change month from the Dropdown");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
+import { MyserviceService } from './myservice.service';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
+import { ChangeTextDirective } from './change-text.directive';
+import { SqrtPipe } from './app.sqrt';
 
 @NgModule({
   declarations: [
+    SqrtPipe,
     AppComponent,
-    NewCmpComponent
+    NewCmpComponent,
+    ChangeTextDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'new-cmp', component: NewCmpComponent }
+    ])
   ],
-  providers: [],
+  providers: [MyserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
